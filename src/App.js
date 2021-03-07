6e30d4a9d43a9d0b954b4aa5197a74152057a92e
Josiah <josiah.james.fowler@gmail.com>
2021-03-07 12:41:01 -0500

    checkout added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './components/Checkout';
+
 
 function App() {
   return (
     //  BEM convention
     <Router>
       <div className="App">
+        <Header />
         <Switch>
           <Route path="/checkout">
-            <Header />
-            <h1>I AM A CHECKOUT, SMASH THE LIKE BUTTON</h1>
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
