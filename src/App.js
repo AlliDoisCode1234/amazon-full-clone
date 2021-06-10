495b3d42f0e420caa8771c04e3beaaee0049d693
Josiah <josiah.james.fowler@gmail.com>
2021-06-10 12:22:28 -0400

    routes added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //  BEM convention
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I AM A CHECKOUT, SMASH THE LIKE BUTTON</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
