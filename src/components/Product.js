883f376d4cab8ffa6ac3ae931fccaa93d8170502
Josiah <josiah.james.fowler@gmail.com>
2021-06-11 11:33:19 -0400

    add to basket feature added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { UseStateValue } from '../StateProvider'
 import './Product.css'
 
-const Product = ({ title, image, price, rating }) => {
+const Product = ({ id, title, image, price, rating }) => {
+
+    const [{ basket }, dispatch] = UseStateValue();
+
+    console.log("this is the basket >>>", basket)
+
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
     return (
         <div className="product">
             <div className="product__info">
@@ -20,7 +39,7 @@ const Product = ({ title, image, price, rating }) => {
             </div>
             <img src={image} alt="" />
 
-            <button className="product__button">Add to Basket</button>
+            <button className="product__button" onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
