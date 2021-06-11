582bc82c5916cf0396b5411d259f199440aada76
Josiah <josiah.james.fowler@gmail.com>
2021-06-11 16:11:24 -0400

    login page styled && auth added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+
+        const signIn = e => {
+            e.preventDefault()
+
+            // some fancy firesbase login stuff
+
+        }
+
+        const register = e => {
+            e.preventDefault()
+
+             // some fancy firesbase login stuff
+
+        }
+
     return (
         <div className="login">
             <Link to='/'>
@@ -19,19 +36,19 @@ const Login = () => {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' />
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' />
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button>Sign In</button>
+                    <button className="login__signInButton" onClick={signIn}>Sign In</button>
                 </form>
 
                 <p>
                     By signing-in you agree to AMAZON FULL CLONE's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button>Create your amazon account</button>
+                <button className="login__registerButton" onClick={register}>Create your amazon account</button>
             </div>
         </div>
     )
