621efbee6074f59addfa272ee8dfc5ef96fc0bd6
Josiah <josiah.james.fowler@gmail.com>
2021-06-12 00:17:41 -0400

    server setup

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,25 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51I6KlaEgMoxwgMH8gJ11Uiy0bK3Sh5XF1SpGSutNBkRpr24UT205PtE7eAiVXN28KWgjLRnb4BBhIaSb5gWqMqIM00ynqsg0QI');
+
+
+// API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+
+// - API routes
+app.get('/', (request, response) => response.status(200).send('hello world'));
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+
+// Example endpoint
+// http://localhost:5001/full-clone-cd754/us-central1/api
